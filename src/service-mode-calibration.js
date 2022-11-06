5be8ba91fe4972ff57ddb50330888290e4c214d7
riky97 <71893803+riky97@users.noreply.github.com>
2022-11-06 15:09:31 +0100

    V 1.0.12 - Add logic erogation time

--- src/service-mode-calibration.js
+++ src/service-mode-calibration.js
@@ -1,5 +1,262 @@
 const sendData = (cmd, item) => {
   switch (cmd) {
+    case 9:
+      if (item === 0) {
+        const value = document.getElementById("water_erogation_time").value;
+        var cs = cmd ^ item ^ value;
+        var result =
+          cmd.toString() +
+          "." +
+          item.toString() +
+          "." +
+          value.toString() +
+          "." +
+          cs.toString() +
+          ".e";
+        console.log(result);
+        var buffReqFl = Buffer.from(result);
+
+        async function listSerialPorts() {
+          await SerialPort.list().then((ports, err) => {
+            if (err) {
+              //document.getElementById('error').textContent = err.message
+              console.log(err);
+              return;
+            }
+
+            console.log("ports", ports);
+
+            if (ports.length === 0) {
+              //document.getElementById('error').textContent = 'No ports discovered'
+              console.log("No ports discovered");
+            }
+            var com;
+            ports.forEach(function (port) {
+              if (port.manufacturer == "FTDI") {
+                if (port.serialNumber == "A100Y8LF") {
+                  com = port.path;
+                }
+              }
+            });
+            console.log(com);
+            serport = new SerialPort({ path: com, baudRate: rate });
+            serport.on("error", function (err) {
+              console.log("Error: ", err.message);
+            });
+
+            serport.write(buffReqFl, (err) => {
+              if (err) {
+                console.log(err.message);
+              } else {
+                console.log("buffReqFl :>> ", buffReqFl);
+              }
+            });
+            serport.on("readable", function () {
+              let buffReceive = serport.read();
+              console.log("receive:", buffReceive);
+              console.log("CMD_MICRO >> ", buffReceive.toString("utf8"));
+              if (buffReceive.toString("utf8") == "30") {
+                cmd_mic_app = buffReceive.toString("utf8");
+                msg = "Value not in range [1-255]!";
+                document
+                  .getElementById("msg")
+                  .classList.remove("alert-success");
+                document.getElementById("msg").textContent = msg;
+                document.getElementById("msg").classList.add("alert-danger");
+              }
+              if (buffReceive.toString("utf8") == "20") {
+                document.getElementById("water_erogation_time").placeholder =
+                  value + " [ml/s * 10]";
+                document.getElementById("water_erogation_time").value = "";
+                const msg = "Value updated!";
+                document.getElementById("msg").classList.remove("alert-danger");
+                document.getElementById("msg").textContent = msg;
+                document.getElementById("msg").classList.add("alert-success");
+              }
+
+              serport.close(function (err) {
+                console.log("port closed", err);
+              }); // close the port after received command from mic
+            });
+          });
+        }
+        setTimeout(() => {
+          listSerialPorts();
+          //closeLoader();
+        }, 100);
+      }
+      if (item === 1) {
+        const value = document.getElementById("flavour_erogation_time").value;
+        var cs = cmd ^ item ^ value;
+        var result =
+          cmd.toString() +
+          "." +
+          item.toString() +
+          "." +
+          value.toString() +
+          "." +
+          cs.toString() +
+          ".e";
+        console.log(result);
+        var buffReqFl = Buffer.from(result);
+
+        async function listSerialPorts() {
+          await SerialPort.list().then((ports, err) => {
+            if (err) {
+              //document.getElementById('error').textContent = err.message
+              console.log(err);
+              return;
+            }
+
+            console.log("ports", ports);
+
+            if (ports.length === 0) {
+              //document.getElementById('error').textContent = 'No ports discovered'
+              console.log("No ports discovered");
+            }
+            var com;
+            ports.forEach(function (port) {
+              if (port.manufacturer == "FTDI") {
+                if (port.serialNumber == "A100Y8LF") {
+                  com = port.path;
+                }
+              }
+            });
+            console.log(com);
+            serport = new SerialPort({ path: com, baudRate: rate });
+            serport.on("error", function (err) {
+              console.log("Error: ", err.message);
+            });
+
+            serport.write(buffReqFl, (err) => {
+              if (err) {
+                console.log(err.message);
+              } else {
+                console.log("buffReqFl :>> ", buffReqFl);
+              }
+            });
+            serport.on("readable", function () {
+              let buffReceive = serport.read();
+              console.log("receive:", buffReceive);
+              console.log("CMD_MICRO >> ", buffReceive.toString("utf8"));
+              if (buffReceive.toString("utf8") == "30") {
+                cmd_mic_app = buffReceive.toString("utf8");
+                msg = "Value not in range [1-255]!";
+                document
+                  .getElementById("msg")
+                  .classList.remove("alert-success");
+                document.getElementById("msg").textContent = msg;
+                document.getElementById("msg").classList.add("alert-danger");
+              }
+              if (buffReceive.toString("utf8") == "20") {
+                document.getElementById("flavour_erogation_time").placeholder =
+                  value + " [ml/s * 100]";
+                document.getElementById("flavour_erogation_time").value = "";
+                const msg = "Value updated!";
+                document.getElementById("msg").classList.remove("alert-danger");
+                document.getElementById("msg").textContent = msg;
+                document.getElementById("msg").classList.add("alert-success");
+              }
+
+              serport.close(function (err) {
+                console.log("port closed", err);
+              }); // close the port after received command from mic
+            });
+          });
+        }
+        setTimeout(() => {
+          listSerialPorts();
+          //closeLoader();
+        }, 100);
+      }
+      if (item === 2) {
+        const value = document.getElementById(
+          "supplement_erogation_time"
+        ).value;
+        var cs = cmd ^ item ^ value;
+        var result =
+          cmd.toString() +
+          "." +
+          item.toString() +
+          "." +
+          value.toString() +
+          "." +
+          cs.toString() +
+          ".e";
+        console.log(result);
+        var buffReqFl = Buffer.from(result);
+
+        async function listSerialPorts() {
+          await SerialPort.list().then((ports, err) => {
+            if (err) {
+              //document.getElementById('error').textContent = err.message
+              console.log(err);
+              return;
+            }
+
+            console.log("ports", ports);
+
+            if (ports.length === 0) {
+              //document.getElementById('error').textContent = 'No ports discovered'
+              console.log("No ports discovered");
+            }
+            var com;
+            ports.forEach(function (port) {
+              if (port.manufacturer == "FTDI") {
+                if (port.serialNumber == "A100Y8LF") {
+                  com = port.path;
+                }
+              }
+            });
+            console.log(com);
+            serport = new SerialPort({ path: com, baudRate: rate });
+            serport.on("error", function (err) {
+              console.log("Error: ", err.message);
+            });
+
+            serport.write(buffReqFl, (err) => {
+              if (err) {
+                console.log(err.message);
+              } else {
+                console.log("buffReqFl :>> ", buffReqFl);
+              }
+            });
+            serport.on("readable", function () {
+              let buffReceive = serport.read();
+              console.log("receive:", buffReceive);
+              console.log("CMD_MICRO >> ", buffReceive.toString("utf8"));
+              if (buffReceive.toString("utf8") == "30") {
+                cmd_mic_app = buffReceive.toString("utf8");
+                msg = "Value not in range [1-255]!";
+                document
+                  .getElementById("msg")
+                  .classList.remove("alert-success");
+                document.getElementById("msg").textContent = msg;
+                document.getElementById("msg").classList.add("alert-danger");
+              }
+              if (buffReceive.toString("utf8") == "20") {
+                document.getElementById(
+                  "supplement_erogation_time"
+                ).placeholder = value + " [gr/s * 100]";
+                document.getElementById("supplement_erogation_time").value = "";
+                const msg = "Value updated!";
+                document.getElementById("msg").classList.remove("alert-danger");
+                document.getElementById("msg").textContent = msg;
+                document.getElementById("msg").classList.add("alert-success");
+              }
+
+              serport.close(function (err) {
+                console.log("port closed", err);
+              }); // close the port after received command from mic
+            });
+          });
+        }
+        setTimeout(() => {
+          listSerialPorts();
+          //closeLoader();
+        }, 100);
+      }
+      break;
     case 100:
       if (item === 0) {
         const value = document.getElementById(
--- src/service-mode.js
+++ src/service-mode.js
@@ -399,89 +399,74 @@ function serviceSidebar(e) {
   }
   if (e == "et") {
     manageCssServiceMode(serviceModeSectionId[3], serviceModeButtonId[3]);
-    let result = "6.e";
+    let result = "202.e";
     console.log(result);
     let buffReqFl = Buffer.from(result);
 
     async function listSerialPorts() {
-      await SerialPort.list().then((ports, err) => {
-        if (err) {
-          //document.getElementById('error').textContent = err.message
-          console.log(err);
-          return;
-        }
-
-        console.log("ports", ports);
-
-        if (ports.length === 0) {
-          //document.getElementById('error').textContent = 'No ports discovered'
-          console.log("No ports discovered");
-        }
-        let com;
-        ports.forEach(function (port) {
-          if (port.manufacturer == "FTDI") {
-            com = port.path;
-          }
-        });
-        console.log(com);
-        serport = new SerialPort({ path: com, baudRate: rate });
-        serport.on("error", function (err) {
-          console.log("Error: ", err.message);
-        });
-
-        serport.write(buffReqFl, (err) => {
+      try {
+        await SerialPort.list().then((ports, err) => {
           if (err) {
-            console.log(err.message);
-          } else {
-            console.log("buffReqFl :>> ", buffReqFl);
+            console.log(err);
+            return;
           }
-        });
-        serport.on("readable", function () {
-          let buffReceive = serport.read();
-          console.log("receive:", buffReceive);
-          console.log("CMD_MICRO >> ", buffReceive.toString("utf8"));
-          if (buffReceive.toString("utf8") == "30") {
-            cmd_mic_app = buffReceive.toString("utf8");
-            msg = "There is an error!";
-            document.getElementById("msg").classList.remove("alert-success");
-            document.getElementById("msg").textContent = msg;
-            document.getElementById("msg").classList.add("alert-danger");
-          } else {
-            for (let i = 1; i <= 5; i++) {
-              if (buffReceive.toString("utf8") == "0") {
-                for (let p = 1; p <= 5; p++) {
-                  document
-                    .getElementById("flavour_" + p.toString())
-                    .classList.remove("btn-bg-tertiary");
-                  document
-                    .getElementById("flavour_" + p.toString())
-                    .classList.add("btn-bg-secondary");
-                }
-              } else {
-                if (buffReceive.toString("utf8") == i.toString()) {
-                  document
-                    .getElementById("flavour_" + i.toString())
-                    .classList.remove("btn-bg-secondary");
-                  document
-                    .getElementById("flavour_" + i.toString())
-                    .classList.add("btn-bg-tertiary");
-                } else {
-                  document
-                    .getElementById("flavour_" + i.toString())
-                    .classList.add("btn-bg-secondary");
-                  document
-                    .getElementById("flavour_" + i.toString())
-                    .classList.remove("btn-bg-tertiary");
-                }
+
+          console.log("ports", ports);
+
+          if (ports.length === 0) {
+            console.log("No ports discovered");
+          }
+          let com;
+          ports.forEach(function (port) {
+            if (port.manufacturer == "FTDI") {
+              if (port.serialNumber == "A100Y8LF") {
+                com = port.path;
               }
             }
-          }
+          });
+          console.log(com);
+          serport = new SerialPort({ path: com, baudRate: rate });
+          serport.on("error", function (err) {
+            console.log("Error: ", err.message);
+          });
 
-          serport.close(function (err) {
-            console.log("port closed", err);
-          }); // close the port after received command from mic
+          serport.write(buffReqFl, (err) => {
+            if (err) {
+              console.log(err.message);
+            } else {
+              console.log("buffReqFl :>> ", buffReqFl);
+            }
+          });
+          serport.on("readable", function () {
+            let buffReceive = serport.read();
+            console.log("CMD_MICRO >> ", buffReceive.toString("utf8"));
+            if (buffReceive.toString("utf8") == "30") {
+              cmd_mic_app = buffReceive.toString("utf8");
+              msg = "There is an error!";
+              document.getElementById("msg").classList.remove("alert-success");
+              document.getElementById("msg").textContent = msg;
+              document.getElementById("msg").classList.add("alert-danger");
+            } else {
+              const cmdErogationTime = buffReceive.toString("utf8").split(".");
+              const valvFlow = cmdErogationTime[0] + " [ml/s * 10]";
+              const pumpFlow = cmdErogationTime[1] + " [ml/s * 100]";
+              const feederFlow = cmdErogationTime[2] + " [gr/s * 100]";
+
+              document.getElementById("water_erogation_time").placeholder =
+                valvFlow;
+              document.getElementById("flavour_erogation_time").placeholder =
+                pumpFlow;
+              document.getElementById("supplement_erogation_time").placeholder =
+                feederFlow;
+            }
+            serport.close(function (err) {
+              console.log("port closed", err);
+            }); // close the port after received command from mic
+          });
         });
-      });
+      } catch (error) {
+        console.log("error :>> ", error);
+      }
     }
     setTimeout(() => {
       listSerialPorts();
